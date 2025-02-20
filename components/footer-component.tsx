56e3a8de27e3f33bd0b4995b896e05ff779c8d9e
Andre Chandra Putra <andrechandra4817@gmail.com>
2025-02-20 14:18:34 +0700

    Updated footer

--- components/footer-component.tsx
+++ components/footer-component.tsx
@@ -10,16 +10,7 @@ export default function Footer() {
       >
         Andre Chandra
       </a>
-      . All rights reserved. <br />
-      Inspired by{' '}
-      <a
-        href="https://theodorusclarence.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="relative w-fit text-white after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:origin-right after:bg-gradient-to-r after:from-yellow-400 after:to-pink-200 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-left"
-      >
-        Theodorus Clarence
-      </a>
+      . All rights reserved.
     </footer>
   )
 }
