29f7d9e3ba195a5caa5021856d74e00985c8c1b4
Andre Chandra Putra <andrechandra4817@gmail.com>
2025-02-28 09:15:59 +0700

    Remove imports

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next'
 import Header from '@/components/header-component'
 import ProjectCard from '@/components/project-card'
-import CodeRain from '@/components/code-rain'
 
 export const metadata: Metadata = {
   title: 'Projects',
